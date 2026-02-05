3a309364fd4e0fc5c828c53f93d20b2942f619cc
Reena <reenasandhu119@gmail.com>
2026-02-05 15:25:09 -0500

    added climb :p

--- src/main/java/frc/robot/subsystems/Climb/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb/Climb.java
@@ -4,11 +4,57 @@
 
 package frc.robot.subsystems.Climb;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimbConstants;
+
+
 public class Climb extends SubsystemBase {
+
+  private final SparkMax climb = new SparkMax(ClimbConstants.climbID, MotorType.kBrushless);
+  
+  private final RelativeEncoder liftEncoder = climb.getEncoder();
+
+  
+
   /** Creates a new Climb. */
-  public Climb() {}
+  public Climb() 
+  {
+  
+  }
+
+  public void setClimb(double climbSpeed)
+  {
+    climb.set(climbSpeed);
+  
+  }
+
+  public void setLeftClimb(double speed)
+  {
+    climb.set(speed);
+  }
+
+  public void setBrakeMode()
+  {
+    climb.setIdleMode(IdleMode.kBrake);
+  }
+
+  public void stopClimb() 
+  {
+    climb.stopMotor();
+  }
 
   @Override
   public void periodic() {
