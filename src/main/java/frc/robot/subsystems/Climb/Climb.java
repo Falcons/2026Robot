7a6a89cce8069f2b2fc95e2fab29393da3ba3d33
Reena <reenasandhu119@gmail.com>
2026-02-05 15:31:57 -0500

    Climb

--- src/main/java/frc/robot/subsystems/Climb/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb/Climb.java
@@ -10,15 +10,10 @@ import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ClimbConstants;
+
 
 
 public class Climb extends SubsystemBase {
