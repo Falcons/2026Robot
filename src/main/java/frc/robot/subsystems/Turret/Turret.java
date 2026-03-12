08eb44d03e671a4d35f0d48346234d5b460e9907
cookie <r1yankhan1135@gmail.com>
2026-03-11 23:56:21 -0400

    long limelight funcrion lol

--- src/main/java/frc/robot/subsystems/Turret/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret/Turret.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems.Turret;
 
+import java.util.Optional;
 import java.util.function.DoubleSupplier;
 
 import com.revrobotics.AbsoluteEncoder;
@@ -14,9 +15,13 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -205,12 +210,43 @@ public class Turret extends SubsystemBase {
 
     double aprilTagOffset[] = LimelightHelpers.getTargetPose_CameraSpace(LimelightConstants.turretLimelight);
     // get angle
-    return -MathUtil.angleModulus(
+    return MathUtil.angleModulus(
       swerve.getHeading().getRadians() + // turret is facing where the bot is facing 
       (turretEncoder.getPosition() - Math.toRadians(90)) + // + its angle -90d offset
       Math.atan2(aprilTagOffset[0], aprilTagOffset[2]) //(TURRET ANGLE) + (APRIL TAG ANGLE)
       // + Math.toRadians(180)
     );
+  }
+
+  /**
+   * get the angle based on limelight with offsets
+   */
+  public double limelightOffsetGlobalRad() { // TODO: long thing
+    if (!LimelightHelpers.lookingAtHub(LimelightConstants.turretLimelight)) return 0.0;
+
+    double tagID = (int) LimelightHelpers.getFiducialID(LimelightConstants.turretLimelight);
+
+    // get april tag pose
+    // AprilTagFieldLayout layout = AprilTagFields.k2026RebuiltWelded //TODO: make better
+    AprilTagFieldLayout layout = AprilTagFields.k2026RebuiltAndymark.loadAprilTagLayoutField();
+    Optional<Pose3d> tagPose = layout.getTagPose((int)tagID);
+
+    Translation2d tagGlobalPos = null;
+    if (tagPose.isPresent()) {
+        tagGlobalPos = tagPose.get().getTranslation().toTranslation2d();
+        tagGlobalPos = new Translation2d(
+          tagGlobalPos.getX() + LimelightConstants.tagOffsets.get(tagID).getX(),
+          tagGlobalPos.getY() + LimelightConstants.tagOffsets.get(tagID).getY());
+    } else {
+      return 0.0;
+    }
+
+    // get angle
+    return MathUtil.angleModulus(
+      Math.atan2(
+        tagGlobalPos.getY() - swerve.getPose().getY(),
+        tagGlobalPos.getX() - swerve.getPose().getX()
+      ));
   }
 
   public double limelightRelativeRad() {
