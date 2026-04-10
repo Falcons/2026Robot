b4a3e31accccc124320a04a2243b8ebc312335f3
Riyan123410 <r1yankhan1135@gmail.com>
2026-04-09 20:15:11 -0400

    comented out unsube misc utils

--- src/main/java/frc/robot/subsystems/MiscUtils.java
+++ src/main/java/frc/robot/subsystems/MiscUtils.java
@@ -4,14 +4,14 @@
 
 package frc.robot.subsystems;
 
-import java.util.List;
+// import java.util.List;
 
-import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.path.PathPlannerPath;
+// import com.pathplanner.lib.commands.PathPlannerAuto;
+// import com.pathplanner.lib.path.PathPlannerPath;
 
-import edu.wpi.first.math.geometry.Pose2d;
+// import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DataLogManager;
-import edu.wpi.first.wpilibj.DriverStation;
+// import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
